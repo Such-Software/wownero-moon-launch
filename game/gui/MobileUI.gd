b1fc2af0478a5bc1f637067b1340259c89d06cea
MyatLynn <myatlinn7447@gmail.com>
2022-12-30 23:20:07 +0630

    add mobile buttons

---
+++ game/gui/MobileUI.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+
+#var player
+#
+#func _ready():
+#	player= get_node("../Rocket/Camera2D")
+#
+#func _process(delta):
+#	rect_position.x=player.position.x
+#	rect_position.y=player.position.y
+#	pass
+
+
+func _on_left_pressed():
+	pass
+
+func _on_right_pressed():
+	pass # Replace with function body.
+
+
+func _on_up_pressed():
+	pass # Replace with function body.
+
+
+func _on_down_pressed():
+	pass # Replace with function body.
